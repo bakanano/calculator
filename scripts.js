305e72ae4f0b83e47f731bbfaa95693d6b33879d
bakanano <narutoz0rs@live.com>
2022-03-02 20:22:26 +1300

    Refactored scripts.js

--- scripts.js
+++ scripts.js
@@ -28,76 +28,10 @@ function operate(operator, a, b) {
             return divide(a, b);
     }
 }
-
-function displayNumber(number) {
-    if (lowerScreen.textContent === "0") {
-        lowerScreen.textContent = "";
-    }
-    lowerScreen.textContent += number;
+function storeNumbers(e) {
+    console.log(e.target.innerText);
 }
 
-function clearScreen() {
-    upperScreen.textContent = "";
-    lowerScreen.textContent = "0";
-}
-
-function deleteNumber() {
-    lowerScreen.textContent = lowerScreen.textContent.slice(0, -1);
-}
-
-function convertKeyboardOperators(operator) {
-    switch(operator) {
-        case "/":
-            operator = "÷";
-            break;
-        case "*":
-            operator = "×";
-            break;
-        case "-":
-            operator = "-";
-            break;
-        case "+":
-            operator = "+";
-            break;
-    }
-
-}
-
-function appendDecimal() {
-    if (lowerScreen.textContent.includes(".")) {
-        return;
-    }
-    lowerScreen.textContent += ".";
-}
-
-function getKeyboardInput(event) {
-    let keyboardInput = event.key;
-    if (keyboardInput >= 0 && keyboardInput <= 9) {
-        displayNumber(event.key);
-    }
-    if (keyboardInput === "Escape") {
-        clearScreen();
-    }
-    if (keyboardInput === "Backspace") {
-        deleteNumber();
-    }
-    if (keyboardInput === "/" || keyboardInput === "*" || keyboardInput === "-" ||
-    keyboardInput === "+") {
-        convertKeyboardOperators(keyboardInput);
-    }
-    if (keyboardInput === ".") {
-        appendDecimal();
-    }
-
-}
-
-document.addEventListener("keydown", getKeyboardInput);
-let upperScreen = document.querySelector(".upperDisplay");
-let lowerScreen = document.querySelector(".lowerDisplay");
-let clearButton = document.querySelector(".clear");
-let backspaceButton = document.querySelector(".backspace");
-let decimalButton = document.querySelector(".decimal");
-
-clearButton.addEventListener("click", clearScreen);
-backspaceButton.addEventListener("click", deleteNumber);
-decimalButton.addEventListener("click", appendDecimal);
+var lowerDisplay = document.querySelector("lowerDisplay");
+var numbersButton = Array.from(document.querySelectorAll(`[data-number]`));
+numbersButton.forEach((number) => number.addEventListener("mouseup", storeNumbers));
